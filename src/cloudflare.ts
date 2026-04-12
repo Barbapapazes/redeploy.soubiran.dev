5a614ddb0de9be7947834d2f6977ad9af0d1f371
barbapapazes <e.soubiran25@gmail.com>
2026-04-12 16:38:45 +0200

    refacor: rename cf_api_token to cloudflare_api_token

--- src/cloudflare.ts
+++ src/cloudflare.ts
@@ -2,5 +2,5 @@ import Cloudflare from 'cloudflare'
 import { env } from 'cloudflare:workers'
 
 export const cloudflare = new Cloudflare({
-  apiToken: env.CF_API_TOKEN,
+  apiToken: env.CLOUDFLARE_API_TOKEN,
 })
